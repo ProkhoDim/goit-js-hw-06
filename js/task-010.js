77e730a4623d09a55ac5515efb168212c1ca84b1
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2019-12-09 19:23:40 +0200

    fix done

--- js/task-010.js
+++ js/task-010.js
@@ -10,16 +10,18 @@ import { users } from './task-object.js';
 
 const getSortedUniqueSkills = users =>
   users
-    .reduce((acc, item) => [...acc, ...item.skills], [])
+    .reduce((acc, item) => {
+      acc = acc.concat(item.skills);
+      return acc;
+    }, [])
     .sort()
-    .reduce((acc, item) => (!acc.includes(item) ? [...acc, item] : acc), []);
-
-// const getSortedUniqueSkills = users =>
-//   users
-//     .map(({ skills }) => skills)
-//     .flat()
-//     .filter((item, index, arr) => arr.indexOf(item) === index)
-//     .sort();
+    .reduce((acc, item) => {
+      if (!acc.includes(item)) {
+        acc.push(item);
+        return acc;
+      }
+      return acc;
+    }, []);
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
