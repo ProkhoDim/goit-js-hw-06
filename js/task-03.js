50a9cd8ed14824d7f90f2f705cb02ef7d77443c4
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2019-12-09 20:03:53 +0200

    fix all

--- js/task-03.js
+++ js/task-03.js
@@ -3,11 +3,14 @@ import { users } from './task-object.js';
 
 // const getUsersWithGender = (users, gender) => users.map(() => users.filter(item => item.gender === gender);
 
-const getUsersWithGender = (users, gender) =>
+const getUsersWithGender = (users, gender) => {
+  const usrWithGender = [];
   users.filter(item => {
     if (item.gender === gender) {
-      return users.push(item.name);
+      usrWithGender.push(item.name);
     }
   });
+  return usrWithGender;
+};
 
 console.log(getUsersWithGender(users, 'male'));
