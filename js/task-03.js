60d002e5140af3701ef1017ec4d8127d8f0de89f
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2019-12-11 22:11:46 +0200

    I understood filter function!

--- js/task-03.js
+++ js/task-03.js
@@ -3,14 +3,7 @@ import { users } from './task-object.js';
 
 // const getUsersWithGender = (users, gender) => users.map(() => users.filter(item => item.gender === gender);
 
-const getUsersWithGender = (users, gender) => {
-  const usrWithGender = [];
-  users.filter(item => {
-    if (item.gender === gender) {
-      usrWithGender.push(item.name);
-    }
-  });
-  return usrWithGender;
-};
+const getUsersWithGender = (users, gender) =>
+  users.filter(item => item.gender === gender).map(item => item.name);
 
 console.log(getUsersWithGender(users, 'male'));
--- js/task-08.js
+++ js/task-08.js
@@ -2,12 +2,7 @@
 import { users } from './task-object.js';
 
 const getUsersWithFriend = (users, friendName) =>
-  users.reduce((acc, item) => {
-    if (item.friends.some(indx => indx === friendName)) {
-      acc.push(item.name);
-      return acc;
-    }
-    return acc;
-  }, []);
+  users.filter(item => item.friends.some(indx => indx === friendName)).map(item => item.name);
+
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
